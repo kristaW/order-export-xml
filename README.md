08b0c61c07aa1477f805d75531f1ed56448e5d99
Krista Wikstroem <krista.wikstroem@blueacorn.com>
2016-06-02 11:39:40 -0400

    updated the code to take advantage of existing varien_filter_Template and it's double brackets. Items works now.

--- README.md
+++ README.md
@@ -26,36 +26,44 @@ The xml layout will not affect the file construction. They can be as deep as nee
 Xml construction is only interested information inside the xml tags.
 
 ### Syntax:
-- {object variables}
 - billing.	// will collect information from order->addresses->billing address info.
+- order.
 - shipping.	// from order->addresses->shipping address info.
-- items.	// currently not working properly but eventually will collect order items.
+
+
+This module relies hevily of **Varien_Filter_Template** modules functionality.
+Each element needs to bee surrounded  with double angle brackets 
+
+#### Example:
+{{var order.entity_id}} 
+{{var billing.firstname}}
+
+### Order Items:
+are looped trough using attribute 'foreach' with value 'items'. See example below.
+The foreach tells the code to loop trough the result values (expecting it to be an array)
+and adds new node with it's children to each item. Foreach attribute value is being used as an keyword 
+to know what is being looped trough. Currently we only have 'items' keyword.
+
+The foreach attribute is removed from result xml.
 
 ### Example:
 ```
 <blueacorn>
   <export>
     <order>
-       <status>status</status>
-       <firstname>customer_firstname</firstname>
-       <lastname>customer_lastname</lastname>
-       <email>customer_email</email>
-       <billing>
-         <city>billing.city</city>
-         <zip>billing.postcode</zip>
-         <address>billing.street</address>
-         <phone>billing.telephone</phone>
-           <shipping>
-             <receiper>shipping.firstname</receiper>
-           </shipping>
-       </billing>
+      <test> {{var order.entity_id}} </test>
+      <address_billing>{{var billing.firstname}} {{var billing.lastname}}</address_billing>
+      <address>{{var shipping.firstname}} {{var shipping.lastname}}</address>
+      <items>
+        <item foreach="items">
+          <sku>{{var sku}}</sku>
+          <name>{{var name}}</name>
+        </item>
+       </items>
     </order>
   </export>
 </blueacorn>
 ```
 
 ## Known Issues:
-- items will break redirecting
-- items will add requested information of bought item on first, but will overwrite it after that.
-- not valid xml (missing xml starting tags)
 - doesn't have proper handling for typing errors occurring in config.xml
--- app/code/local/BlueAcorn/OrderExport/Model/Export.php
+++ app/code/local/BlueAcorn/OrderExport/Model/Export.php
@@ -1,27 +1,35 @@
 <?php
 /**
- * Short description for file
- *
- * Long description for file (if any)...
+ * Exports order into an xml file.
  *
  * @package     BlueAcorn\OrderExport
- * @version     $Id:$
+ * @version     1.0.0.2
  * @author      Blue Acorn, Inc. <code@blueacorn.com>
  * @copyright   Copyright © 2016 Blue Acorn, Inc.
  */
 class BlueAcorn_OrderExport_Model_Export {
-    
+    public $formater = '';
+    public $order = '';
+
+    /**
+     * Reads xml node from config
+     * @param $item
+     * @return Mage_Core_Model_Config_Element
+     */
     public function collectSettings($item) {
 
-        $config = Mage::getConfig()->getNode('blueacorn/export/'.$item);
-
+        $config = Mage::getConfig()->getNode('blueacorn/export_template/'.$item);
         return $config;
     }
 
     /**
-     * @todo: add header to file and form the file to be valid xml
+     * Outputs xml file into folder var/blueacorn/export
+     *
+     * @param $filename
+     * @param $content
+     * @throws Exception
      */
-    public function exportToXML($filename, $order) {
+    public function exportToXML($filename, $content) {
 
         $io = new Varien_Io_File();
         
@@ -29,90 +37,95 @@ class BlueAcorn_OrderExport_Model_Export {
         $io->checkAndCreateFolder($dir);
 
         $xmlPath = $dir . DS . $filename ;
-
-        $xmlData = $order->asNiceXml($filename);
+        $xmlData = '<?xml version="1.0" encoding="UTF-8"?>'."\n";
+        $xmlData .= $content->asNiceXml($filename);
         
         $io->open(array('path' => $dir));
-
         $io->write($xmlPath, $xmlData);
-
     }
 
-    public function constructXML($config, $order) {
-
-        foreach (get_object_vars($config) as $key => $value) {
-
-            if(strpos($value, '.') !== false) {
-                $a = explode(".", $value);
-                $m = $order;
-                $c = 'order';
-                for($i = 0; $i < $a; $i++) {
-
-                    if(is_string($m)) {
-//                        $config[$key] = $m;
-                        $config->setNode($key, $m);
-
-                        break;
-                    }
-                    if($m === null || $m === '') {
-                        $config->setNode($key, $m);
-
-                        break;
-                    }
-
-                    $c .= '->'.$a[$i];
-
-                    switch ($a[$i]) {
-                        case "billing":
-                            $m = $m->getBillingAddress();
-                            break;
-                        case "shipping":
-                            $m = $m->getShippingAddress();
-                            break;
-                        case "items":
-                            $m = $m->getItemsCollection()->getItems();
-                            foreach (get_object_vars($config) as $me => $item) {
-                                $config->setNode($me, str_replace('items.', '', $item));
-                            }
-
-                            for ($j = 0; $j < count($m); $j++) {
-
-                                $this->constructXML($config, $m[$j]);
-                            }
-                            break;
-                        /**
-                         * @todo: needs proper error handling (typos)
-                         */
-                        default:
-                            $m = $m->{$a[$i]};
-                            break;
-                    }
-                }
-            } elseif ($order[$value]) {
-                $config->setNode($key, $order[$value]);
-            } elseif (is_object($value)) {
-                $this->constructXML($value, $order);
-            } else {
-                if(is_object($value)) {
-                    var_dump("hello I'm object");
-                } elseif (is_array($value)) {
-                    var_dump("hello I'm array");
-                } else {
-                    var_dump("hello I'm either string");
-                    var_dump("or I'm unknown at the moment");
-                    break;
-                }
-            }
-        }
-    }
-
+    /**
+     * Lauches when order is being submitted into the system
+     * @param $observer
+     */
     public function observeOrderSave($observer) {
 
-        $config = $this->collectSettings('order');
-        $order = $observer->getOrder();
-        $filename = $order->getIncrementId() .".xml";
+        $template = $this->collectSettings('order');
+        $this->order = $observer->getOrder();
+        $filename = $this->order->getIncrementId() .".xml";
 
-        $this->constructXML($config, $order);
-        $this->exportToXML($filename, $config);
+        $data = array(
+            'order' => $this->order,
+            'shipping' => $this->order->getShippingAddress(),
+            'billing' => $this->order->getBillingAddress()
+        );
+
+        $this->formater = new Varien_Filter_Template();
+
+        $this->formater->setVariables($data);
+
+        $this->constructXML($template);
+        $this->exportToXML($filename, $template);
+    }
+
+    /**
+     * Collects data into elements
+     * @param $template
+     */
+    public function constructXML($template) {
+
+        if(is_object($template)) {
+            foreach (get_object_vars($template->children()) as $key => $value ) {
+
+                if(is_object($value)) {
+
+                    /**
+                     * found attribute foreach, loop this part of xml until the loop ends
+                     */
+                    if ($value['foreach']) {
+
+                        /**
+                         * Corresponds to attribute value. Uses that information
+                         * to know what is being looped
+                         */
+                        $parent = (string)$value['foreach'];
+
+                        if($parent === 'items') {
+
+                            $items = $this->order->getAllVisibleItems();
+                            $default = $value;
+
+                            foreach ($items as $itemKey => $item) {
+
+                                $value = $default;
+                                $value['id'] = $itemKey;
+                                $itemNode = $template->addChild($key);
+                                $itemNode ->addAttribute('id', $itemKey);
+
+                                foreach (get_object_vars($value->children()) as $valueKey => $valueChild ) {
+                                    $valueChild = str_replace( array('{{var ','}}') , '', $valueChild);
+
+                                    if ($item->getData($valueChild)) {
+                                        $itemNode->addChild($valueKey, $item->getData($valueChild));
+                                    }
+                                }
+                            }
+                            unset($template->item[0]);
+                        }
+
+                    } else {
+                        $this->constructXML($value);
+                    }
+                }
+
+                /** filter does the actual data mining. if result is string we can set it into our xml */
+                $result = $this->formater->filter($value);
+                if(is_string($result)) {
+                    $template->$key = $result;
+                }
+            }
+        } else {
+            $this->formater->filter($template);
+        }
     }
 }
